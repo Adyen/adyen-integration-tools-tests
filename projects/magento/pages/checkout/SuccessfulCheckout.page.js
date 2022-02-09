42db9d1ffb59cdffb0d20d75f45c4acaccaa547c
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-02-09 19:13:36 +0100

    Add extra selector to ensure successful checkout

--- projects/magento/pages/checkout/SuccessfulCheckout.page.js
+++ projects/magento/pages/checkout/SuccessfulCheckout.page.js
@@ -6,9 +6,13 @@ export class SuccessfulCheckoutPage extends BasePage {
     this.page = page;
 
     this.pageTitle = page.locator(".page-title .base");
+    this.continueButton = page.locator(
+      "//*[@class = 'action primary continue']//*[text()='Continue Shopping']"
+    );
   }
 
   async waitforNavigaton() {
-    await this.page.waitForNavigation({waitUntil:"load"});
+    await this.page.waitForNavigation({ waitUntil: "load" });
+    await this.continueButton.waitFor({ state: "visible", timeout: 5000 });
   }
 }
