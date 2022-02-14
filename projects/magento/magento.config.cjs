faedc17c8ad587344e1963787b7ae9df66a3d36a
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-02-14 19:49:00 +0100

    Enable parallellism on CI

--- projects/magento/magento.config.cjs
+++ projects/magento/magento.config.cjs
@@ -26,7 +26,7 @@ const config = {
   retries: process.env.CI ? 2 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 5 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
