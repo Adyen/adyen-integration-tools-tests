d1d2f85e1184650ac5ad7f7dc75e0ce8451f3928
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-02-22 11:18:38 +0100

    Remove redundant expect

--- projects/magento/tests/KlarnaPayLaterPayment.spec.js
+++ projects/magento/tests/KlarnaPayLaterPayment.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import KlarnaPaymentPage from "../../common/KlarnaPaymentPage.js";
 import PaymentResources from "../../data/PaymentResources.js";
 import {
