6ab877797e0be8209c21c6bbf19790086a9b25ee
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2022-03-10 16:30:59 +0100

    Simplify payment details page for less repeat

--- projects/magento/pageObjects/checkout/PaymentDetails.page.js
+++ projects/magento/pageObjects/checkout/PaymentDetails.page.js
@@ -1,4 +1,3 @@
-import { OneyPaymentPage } from "../../../common/OneyPaymentPage.js";
 import { BancontactCardComponents } from "./BancontactCardComponents.js";
 import { CreditCardComponents } from "./CreditCardComponents.js";
 import { GenericGiftCardComponents } from "./GenericGiftCardComponents.js";
@@ -8,6 +7,7 @@ import { OneyComponents } from "./OneyComponents.js";
 import { PayPalComponents } from "./PayPalComponents.js";
 import { SepaDirectDebitComponents } from "./SepaDirectDebitComponents.js";
 import { ClearPayComponents } from "./ClearPayComponents.js";
+import { BoletoComponents } from "./BoletoComponents.js";
 
 export class PaymentDetailsPage {
   constructor(page) {
@@ -22,6 +22,7 @@ export class PaymentDetailsPage {
     this.genericGiftCardRadioButton = page.locator("#adyen_genericgiftcard");
     this.oney3RadioButton = page.locator("#adyen_facilypay_3x");
     this.clearPayRadioButton = page.locator("#adyen_clearpay");
+    this.boletoRadioButton = page.locator("#adyen_boleto");
 
     this.paymentSummaryLoadingSpinner = page.locator(
       ".opc-sidebar .loading-mask"
@@ -31,65 +32,67 @@ export class PaymentDetailsPage {
 
   async selectCreditCard() {
     await this.creditCardRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new CreditCardComponents(this.page);
   }
 
   async selectIDeal() {
     await this.idealRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new IDealComponents(this.page);
   }
 
   async selectPayPal() {
     await this.payPalRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new PayPalComponents(this.page);
   }
 
   async selectKlarnaPayLater() {
     await this.klarnaPayLaterRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new KlarnaPayLaterComponents(this.page);
   }
 
   async selectBancontactCard() {
     await this.bancontactCardRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new BancontactCardComponents(this.page);
   }
 
   async selectSepaDirectDebit() {
     await this.sepaDirectDebitRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new SepaDirectDebitComponents(this.page);
   }
 
   async selectGiftCard() {
     await this.genericGiftCardRadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new GenericGiftCardComponents(this.page);
   }
 
   async selectOney() {
     await this.oney3RadioButton.click();
-    await this.waitForPaymentSummaryLoader();
-    await this.activePaymentMethod.scrollIntoViewIfNeeded();
+    await this.waitForPaymentMethodReady();
     return new OneyComponents(this.page);
   }
 
   async selectClearPay() {
     await this.clearPayRadioButton.click();
+    await this.waitForPaymentMethodReady();
+    return new ClearPayComponents(this.page);
+  }
+
+  async selectBoleto() {
+    await this.boletoRadioButton.click();
+    await this.waitForPaymentMethodReady();
+    return new BoletoComponents(this.page);
+  }
+
+  async waitForPaymentMethodReady() {
     await this.waitForPaymentSummaryLoader();
     await this.activePaymentMethod.scrollIntoViewIfNeeded();
-    return new ClearPayComponents(this.page);
   }
 
   async waitForPaymentSummaryLoader() {
