b9102006989bfe95524183558bd104d1acbeeadd
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-03-10 23:27:57 +0100

    Increase retry to 3 on CI

--- projects/magento/magento.config.cjs
+++ projects/magento/magento.config.cjs
@@ -23,7 +23,7 @@ const config = {
   forbidOnly: !!process.env.CI,
 
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
 
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 3 : undefined,
