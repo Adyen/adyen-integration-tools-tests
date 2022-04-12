79afa4518c6b5b5284843132c24329b53a4cf8ea
cenkk <cenk.kucukiravul@adyen.com>
2022-04-12 16:27:49 +0200

    Make shipping page selectors more robust

--- projects/magento/pageObjects/checkout/ShippingDetails.page.js
+++ projects/magento/pageObjects/checkout/ShippingDetails.page.js
@@ -6,17 +6,17 @@ export class ShippingDetails {
 
     // Shipping Section Locators
     this.emailInput = page.locator(
-      "//*[@id='customer-email' and @type='email' and @aria-required='true']"
+      "#checkout-step-shipping #customer-email"
     );
-    this.firstNameInput = page.locator("input[name='firstname']");
-    this.lastNameInput = page.locator("input[name='lastname']");
-    this.addressInput = page.locator("input[name='street[0]']");
-    this.countrySelector = page.locator("select[name='country_id']");
-    this.stateProvinceDropdown = page.locator("select[name='region_id']");
-    this.stateProvinceField = page.locator("input[name='region']");
-    this.cityInput = page.locator("input[name='city']");
-    this.postCodeInput = page.locator("input[name='postcode']");
-    this.phoneNumberInput = page.locator("input[name='telephone']");
+    this.firstNameInput = page.locator("#checkout-step-shipping input[name='firstname']");
+    this.lastNameInput = page.locator("#checkout-step-shipping input[name='lastname']");
+    this.addressInput = page.locator("#checkout-step-shipping input[name='street[0]']");
+    this.countrySelector = page.locator("#checkout-step-shipping select[name='country_id']");
+    this.stateProvinceDropdown = page.locator("#checkout-step-shipping select[name='region_id']");
+    this.stateProvinceField = page.locator("#checkout-step-shipping input[name='region']");
+    this.cityInput = page.locator("#checkout-step-shipping input[name='city']");
+    this.postCodeInput = page.locator("#checkout-step-shipping input[name='postcode']");
+    this.phoneNumberInput = page.locator("#checkout-step-shipping input[name='telephone']");
     this.shippingMethodRadioButton = page
       .locator("#checkout-shipping-method-load input[type='radio']")
       .first();
@@ -25,17 +25,19 @@ export class ShippingDetails {
     );
   }
 
-  async goTo() {
+  async goTo(waitForAnimation = true) {
     // Only works with a non-empty shopping cart
     await this.page.goto("/checkout/#shipping");
-    await new AnimationHelper(this.page).waitForAnimation();
+    if (waitForAnimation != false){
+      await new AnimationHelper(this.page).waitForAnimation();
+    }
   }
 
   async fillShippingDetails(user) {
-    await this.emailInput.type(user.email);
-    await this.firstNameInput.type(user.firstName);
-    await this.lastNameInput.type(user.lastName);
-    await this.addressInput.type(user.street);
+    await this.emailInput.fill(user.email);
+    await this.firstNameInput.fill(user.firstName);
+    await this.lastNameInput.fill(user.lastName);
+    await this.addressInput.fill(user.street);
 
     await this.countrySelector.selectOption(user.countryCode);
     await new AnimationHelper(this.page).waitForAnimation();
@@ -43,13 +45,13 @@ export class ShippingDetails {
     if (user.stateOrProvince != undefined) {
       await this.stateProvinceDropdown.selectOption(user.stateOrProvince);
     } else if (user.stateCode != undefined) {
-      await this.stateProvinceField.type(user.stateCode);
+      await this.stateProvinceField.fill(user.stateCode);
     }
 
-    await this.cityInput.type(user.city);
-    await this.postCodeInput.type(user.postCode);
+    await this.cityInput.fill(user.city);
+    await this.postCodeInput.fill(user.postCode);
 
-    await this.phoneNumberInput.type(user.phoneNumber);
+    await this.phoneNumberInput.fill(user.phoneNumber);
     await this.shippingMethodRadioButton.check();
   }
 
