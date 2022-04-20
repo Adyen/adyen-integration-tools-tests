40fedf75241ba2685ce609bf633ce636c963694d
Michael Paul <michael@michaelpaul.com.br>
2022-04-20 16:53:47 +0200

    Update magentoCIContainer.config.cjs

--- projects/magento/magentoCIContainer.config.cjs
+++ projects/magento/magentoCIContainer.config.cjs
@@ -41,7 +41,7 @@ const config = {
     ignoreHTTPSErrors: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
