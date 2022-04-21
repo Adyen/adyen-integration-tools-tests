02e318435e05f106af72ae7ab2edde4d0ec1e221
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-04-21 14:57:54 +0200

    Retain the traces only in case of a failure

--- projects/magento/magentoCIContainer.config.cjs
+++ projects/magento/magentoCIContainer.config.cjs
@@ -41,7 +41,7 @@ const config = {
     ignoreHTTPSErrors: true,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on",
+    trace: "retain-on-failure",
   },
 
   /* Configure projects for major browsers */
