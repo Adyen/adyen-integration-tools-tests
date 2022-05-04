93ba58be2d366d544d154de1a8434a0acb7f59f3
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-05-04 17:29:28 +0200

    Reduce workers to 2

--- projects/magento/magento.config.cjs
+++ projects/magento/magento.config.cjs
@@ -26,7 +26,7 @@ const config = {
   retries: process.env.CI ? 3 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 3 : undefined,
+  workers: process.env.CI ? 2 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
