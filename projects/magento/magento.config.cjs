db6b435782b5dc801034acd62dfb1eab218bd18d
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-05-06 14:07:10 +0200

    Tweak worker count and retries

--- projects/magento/magento.config.cjs
+++ projects/magento/magento.config.cjs
@@ -23,10 +23,10 @@ const config = {
   forbidOnly: !!process.env.CI,
 
   /* Retry on CI only */
-  retries: process.env.CI ? 3 : 0,
+  retries: process.env.CI ? 1 : 0,
 
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 3 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
