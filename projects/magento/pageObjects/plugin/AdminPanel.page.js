e7fd747211a23d9e6cf001949e3d56e5e0e0c4cb
cenkk <cenk.kucukiravul@adyen.com>
2022-07-20 13:39:08 +0200

    Fix admin tests

--- projects/magento/pageObjects/plugin/AdminPanel.page.js
+++ projects/magento/pageObjects/plugin/AdminPanel.page.js
@@ -18,10 +18,17 @@ export class AdminPanelPage {
     this.payByLinkSelector = page.locator("#p_method_adyen_pay_by_link");
     this.submitOrderButton = page.locator("#submit_order_top_button");
     this.paymentLink = page.locator("a[rel='noopener']");
+
+    // Sidebar locators
+    this.salesLink = page.locator("#menu-magento-sales-sales");
+    this.salesSideMenu = page.locator(".item-sales-operation");
+
+    this.orderLink = this.salesSideMenu.locator("//span[text()='Orders']")
   }
 
-  async goToOrdersPage(page) {
-    await page.goto("/admin/sales/order");
+  async goToOrdersPage() {
+    await this.salesLink.click();
+    await this.orderLink.click();
   }
 
   async waitForAdminPanelAnimation(page) {
