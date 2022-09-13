c51bc7f24f25666edddd07fb99c8727172710be9
cenkk <cenk.kucukiravul@adyen.com>
2022-09-13 18:35:16 +0200

    Make CC holder name selector more generic

--- projects/magento/pageObjects/checkout/CreditCardComponents.js
+++ projects/magento/pageObjects/checkout/CreditCardComponents.js
@@ -6,8 +6,9 @@ export class CreditCardComponents {
     this.errorMessage = page.locator("#adyen-cc-form .message-error");
 
     this.holderNameInput = page.locator(
-      "#payment_form_adyen_cc .adyen-checkout__card__holderName input"
+      ".adyen-checkout__card__holderName input"
     );
+
     this.cardNumberInput = page
       .frameLocator(".adyen-checkout__card__cardNumber__input iframe")
       .locator(".input-field");
