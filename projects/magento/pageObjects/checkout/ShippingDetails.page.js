f544df09599e0d380323d4c9b6d2751b42e435e4
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2022-09-29 14:49:46 +0200

    Remove redundant hard wait

--- projects/magento/pageObjects/checkout/ShippingDetails.page.js
+++ projects/magento/pageObjects/checkout/ShippingDetails.page.js
@@ -66,9 +66,6 @@ export class ShippingDetails {
     await this.page.waitForLoadState("domcontentloaded", { timeout: 10000 });
     await this.page.waitForLoadState("networkidle", { timeout: 10000 });
 
-    // Unavoidable hard wait until the payment method render issue is resolved
-    await new Promise(r => setTimeout(r, 1500));
-
     await this.clickNextButton();
     await new AnimationHelper(this.page).waitForAnimation();
   }
