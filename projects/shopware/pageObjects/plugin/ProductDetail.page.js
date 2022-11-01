620a743f8778510f6f2518ab306c8b7162b8a037
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2022-11-01 11:50:01 +0100

    Add product detail page

---
+++ projects/shopware/pageObjects/plugin/ProductDetail.page.js
@@ -0,0 +1,33 @@
+
+import { BasePage } from "./Base.page";
+
+export class ProductDetailPage extends BasePage {
+    constructor(page) {
+        super(page);
+        this.page = page;
+
+
+        this.productDetailContainer = page.locator(".product-detail-buy");
+
+        this.addToCartButton = this.productDetailContainer.locator(".btn-buy");
+        this.productDetailQuantityDropdown = this.productDetailContainer.locator(".product-detail-quantity-select");
+
+    }
+
+    async changeProductQuantity(quantity) {
+        await this.productDetailQuantityDropdown.selectOption(`${quantity}`);
+    }
+
+    async clickAddToCart() {
+        await this.addToCartButton.click();
+    }
+
+    async addItemToCart(itemURL, count = 1) {
+        await this.page.goto(`/${itemURL}`);
+        if (count > 1) {
+            await this.changeProductQuantity(count);
+        }
+        await this.clickAddToCart();
+    }
+
+}
