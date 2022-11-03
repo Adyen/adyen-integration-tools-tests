8e7ecd3f7ae4ffd35be42afdf92caef7ec7b7515
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2022-11-03 17:47:30 +0100

    Run only backoffice tests

--- projects/magento/magentoCIContainer.config.cjs
+++ projects/magento/magentoCIContainer.config.cjs
@@ -45,15 +45,15 @@ const config = {
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: "user",
-      testDir: "./tests/",
-      testIgnore: "**/backoffice/**",
-      use: {
-        browserName: "chromium",
-        trace: "retain-on-failure",
-      }
-    },
+    // {
+    //   name: "user",
+    //   testDir: "./tests/",
+    //   testIgnore: "**/backoffice/**",
+    //   use: {
+    //     browserName: "chromium",
+    //     trace: "retain-on-failure",
+    //   }
+    // },
     {
       name: "backoffice",
       testDir: "./tests/backoffice",
