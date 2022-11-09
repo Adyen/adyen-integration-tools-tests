4eb7718f57e2af6438ea2a8059e23e8a693c5a11
Cenk Kucukiravul <cenkiravul35@gmail.com>
2022-11-09 15:41:18 +0100

    [PW-7432] Fix E2E tests (#37)
    * Assert for HMAC error message on local env only
    
    * Add extra waits to compensate slow Actions runner
    
    * Make sure that the api key is stringified
    
    Co-authored-by: Cenk Kucukiravul <cenk.kucukiravul@adyen.com>

--- projects/magento/pageObjects/plugin/AdminAdyenConfig.page.js
+++ projects/magento/pageObjects/plugin/AdminAdyenConfig.page.js
@@ -76,7 +76,7 @@ export class AdminAdyenConfigPage extends AdminPanelPage {
     }
 
     await this.apiKeyInput.fill("");
-    await this.apiKeyInput.type(apiKey);
+    await this.apiKeyInput.type(`${apiKey}`);
     await this.nextButton.scrollIntoViewIfNeeded();
     await this.nextButton.click();
 
