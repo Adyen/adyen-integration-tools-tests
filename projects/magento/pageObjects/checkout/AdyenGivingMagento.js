daba783ee94c7a8188fdf350e6053ee8dc75c774
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2022-11-10 16:22:05 +0100

    Remove duplicated code block

--- projects/magento/pageObjects/checkout/AdyenGivingMagento.js
+++ projects/magento/pageObjects/checkout/AdyenGivingMagento.js
@@ -1,17 +1,8 @@
-import { expect } from "@playwright/test";
 import { AdyenGivingComponents } from "../../../common/checkoutComponents/AdyenGivingComponents.js";
 export class AdyenGivingMagento extends AdyenGivingComponents {
   constructor(page) {
     super(page);
     this.page = page;
-
-    this.DonationMessage = this.adyenGivingContainer.locator(
-      ".adyen-checkout__status__text"
-    );
-  }
-
-  async verifySuccessfulDonationMessage() {
-    await expect(this.DonationMessage).toHaveText("Thanks for your support!");
   }
 
   async verifyDeclineRedirection() {
