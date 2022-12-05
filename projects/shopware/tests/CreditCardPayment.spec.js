ea11931f4acd3c6f47beba807c919e90c1a1ad29
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2022-12-05 16:27:58 +0100

    Add 3ds1, 3ds2 and negative scenarios for SW6 CC

--- projects/shopware/tests/CreditCardPayment.spec.js
+++ projects/shopware/tests/CreditCardPayment.spec.js
@@ -1,9 +1,12 @@
 import { test } from "@playwright/test";
+import { ThreeDS2PaymentPage } from "../../common/redirect/ThreeDS2PaymentPage.js";
+import { ThreeDSPaymentPage } from "../../common/redirect/ThreeDSPaymentPage.js";
 import PaymentResources from "../../data/PaymentResources.js";
 import {
     doPrePaymentChecks,
     goToShippingWithFullCart,
     proceedToPaymentAs,
+    verifyFailedPayment,
     verifySuccessfulPayment
 } from "../helpers/ScenarioHelper.js";
 import { PaymentDetailsPage } from "../pageObjects/plugin/PaymentDetails.page.js";
@@ -30,6 +33,90 @@ test.describe.parallel("Payment via credit card", () => {
 
         await verifySuccessfulPayment(page);
 
+    });
+
+    test("with 3Ds1 should succeed", async ({ page }) => {
+
+        await makeCreditCardPayment(
+            page,
+            users.regular,
+            paymentResources.visa3DS1,
+            paymentResources.expDate,
+            paymentResources.cvc
+        );
+
+        await new ThreeDSPaymentPage(page).validate3DS(
+            paymentResources.threeDSCorrectUser,
+            paymentResources.threeDSCorrectPassword
+        );
+        await verifySuccessfulPayment(page);
+    });
+
+    test("with wrong 3Ds1 credentials should fail", async ({ page }) => {
+
+        await makeCreditCardPayment(
+            page,
+            users.regular,
+            paymentResources.visa3DS1,
+            paymentResources.expDate,
+            paymentResources.cvc
+        );
+
+        await new ThreeDSPaymentPage(page).validate3DS(
+            paymentResources.threeDSCorrectUser,
+            paymentResources.threeDSWrongPassword
+        );
+
+        await verifyFailedPayment(page);
+    });
+
+    test("with 3Ds2 should succeed", async ({ page }) => {
+
+        await makeCreditCardPayment(
+            page,
+            users.regular,
+            paymentResources.masterCard3DS2,
+            paymentResources.expDate,
+            paymentResources.cvc
+        );
+
+        await new ThreeDS2PaymentPage(page).validate3DS2(
+            paymentResources.threeDSCorrectPassword
+        );
+
+        await verifySuccessfulPayment(page);
+    });
+
+    test("with wrong 3Ds2 credentials should fail", async ({ page }) => {
+
+        await makeCreditCardPayment(
+            page,
+            users.regular,
+            paymentResources.masterCard3DS2,
+            paymentResources.expDate,
+            paymentResources.cvc
+        );
+
+        await new ThreeDS2PaymentPage(page).validate3DS2(
+            paymentResources.threeDSWrongPassword
+        );
+
+        await verifyFailedPayment(page);
+    });
+
+    test("with 3Ds2 should abort the payment with correct message when cancelled", async ({ page }) => {
+
+        await makeCreditCardPayment(
+            page,
+            users.regular,
+            paymentResources.masterCard3DS2,
+            paymentResources.expDate,
+            paymentResources.cvc
+        );
+
+        await new ThreeDS2PaymentPage(page).clickCancel();
+
+        await verifyFailedPayment(page);
     });
 
 });
