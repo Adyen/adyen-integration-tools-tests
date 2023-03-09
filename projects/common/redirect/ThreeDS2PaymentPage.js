a6eef2b8e4b8f0406469fc7319f7121f9e12cf7e
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2023-03-09 17:19:02 +0100

    Update 3Ds2 locators for the new simulator

--- projects/common/redirect/ThreeDS2PaymentPage.js
+++ projects/common/redirect/ThreeDS2PaymentPage.js
@@ -1,10 +1,7 @@
 export class ThreeDS2PaymentPage {
   constructor(page) {
     this.page = page;
-    this.threeDS2ModalFrame = page.locator("iframe[name='threeDSIframe']");
-    this.threeDS2Iframe = page.frameLocator(
-      ".adyen-checkout__threeds2__challenge iframe"
-    ).frameLocator("iframe");
+    this.threeDS2Iframe = page.frameLocator("iframe[name='threeDSIframe']");
     this.threeDS2PasswordInput = this.threeDS2Iframe.locator(
       "input[name='answer']"
     );
@@ -21,14 +18,14 @@ export class ThreeDS2PaymentPage {
   }
 
   async fillThreeDS2PasswordAndSubmit(answer) {
-    await this.threeDS2ModalFrame.waitFor({ state: "visible", timeout: 10000 });
+    await this.threeDS2PasswordInput.waitFor({ state: "visible", timeout: 10000 });
     await this.threeDS2PasswordInput.click();
     await this.threeDS2PasswordInput.type(answer);
     await this.threeDS2SubmitButton.click();
   }
 
   async clickCancel() {
-    await this.threeDS2ModalFrame.waitFor({ state: "visible", timeout: 10000 });
+    await this.threeDS2CancelButton.waitFor({ state: "visible", timeout: 10000 });
     await this.threeDS2CancelButton.click();
   }
 
