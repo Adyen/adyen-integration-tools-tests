65f6cc51d646ab3e5bdcc7bef09bdb0b57b2e326
Cenk Kucukiravul <cenkiravul35@gmail.com>
2023-05-22 16:17:43 +0200

    [PW-8407] Expand E2E coverage for 3DS2 stored cards (#64)
    * Enable selection of multiple stored cards
    
    * Add 3Ds2 coverage for stored cards
    
    ---------
    
    Co-authored-by: Cenk Kucukiravul <cenk.kucukiravul@adyen.com>

--- projects/magento/tests/loggedIn/StoredCardPayment.spec.js
+++ projects/magento/tests/loggedIn/StoredCardPayment.spec.js
@@ -9,17 +9,48 @@ import {
   placeOrder,
 } from "../../../magento/helpers/ScenarioHelper.js";
 import { CreditCardComponentsMagento } from "../../pageObjects/checkout/CreditCardComponentsMagento.js";
+import { ThreeDS2PaymentPage } from "../../../common/redirect/ThreeDS2PaymentPage.js";
 
 const paymentResources = new PaymentResources();
 const magentoSampleUser = paymentResources.sampleRegisteredUser;
 const users = paymentResources.guestUser;
 
+/* No parallelism due to usage of same user account
+since it will cause the cart to reset */
 test.describe("Payment via stored credit card", () => {
   test.beforeEach(async ({ page }) => {
     await loginAs(page, magentoSampleUser);
     await goToShippingWithFullCart(page);
     await proceedToPaymentAs(page, undefined, false);
 
+    
+  });
+
+  test("should succeed with 3Ds2", async ({ page }) => {
+    await makeCreditCardPayment(
+      page,
+      users.regular,
+      paymentResources.masterCard3DS2,
+      paymentResources.expDate,
+      paymentResources.cvc,
+      true
+    );
+    await new ThreeDS2PaymentPage(page).validate3DS2(
+      paymentResources.threeDSCorrectPassword
+    );
+    await verifySuccessfulPayment(page);
+
+    await goToShippingWithFullCart(page);
+    await proceedToPaymentAs(page, undefined, false);
+
+    await makeVaultPayment(page, paymentResources.masterCard3DS2, paymentResources.cvc);
+    await new ThreeDS2PaymentPage(page).validate3DS2(
+      paymentResources.threeDSCorrectPassword
+    );
+    await verifySuccessfulPayment(page);
+  });
+
+  test("should succeed with no 3Ds", async ({ page }) => {
     await makeCreditCardPayment(
       page,
       users.regular,
@@ -29,13 +60,11 @@ test.describe("Payment via stored credit card", () => {
       true
     );
     await verifySuccessfulPayment(page);
-  });
 
-  test("should succeed", async ({ page }) => {
     await goToShippingWithFullCart(page);
     await proceedToPaymentAs(page, undefined, false);
 
-    await makeVaultPayment(page, paymentResources.cvc);
+    await makeVaultPayment(page, paymentResources.masterCardWithout3D, paymentResources.cvc);
     await verifySuccessfulPayment(page);
   });
 });
@@ -63,8 +92,8 @@ async function makeCreditCardPayment(
   await placeOrder(page);
 }
 
-async function makeVaultPayment(page, cvc) {
-  await new PaymentDetailsPage(page).selectVault();
+async function makeVaultPayment(page, creditCardNumber, cvc) {
+  await new PaymentDetailsPage(page).selectVault(creditCardNumber.slice(-4));
   await new CreditCardComponentsMagento(page.locator(".payment-method._active")).fillCVC(cvc);
 
   await placeOrder(page);
