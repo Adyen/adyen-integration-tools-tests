a1c3a8dec77021601dab3bb733c42ea33e1b4483
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2023-08-24 17:08:49 +0200

    Add Google Pay page objects and functions

--- projects/common/redirect/GooglePayPage.js
+++ projects/common/redirect/GooglePayPage.js
@@ -1,35 +1,61 @@
 export class GooglePayPage {
   constructor(page) {
     this.page = page;
-    this.emailInput = page.locator("#username");
+    
+    this.loginHeader = page.locator("#headingText")
+    this.emailInput = page.locator("input[type='email']");
     this.nextButton = page.getByRole('button', { name: 'Next' });
 
-    this.passwordInput = page.locator("input[type='password']");
+    this.passwordInput = page.locator("input[name='Passwd']");
 
-    this.payButton = page.locator(".jfk-button");
+    this.paymentIframe = page.frameLocator("iframe[allow='camera']")
+    this.payButton = this.paymentIframe.locator(".jfk-button").first();
   }
 
   async fillUsername(username) {
+    await this.loginHeader.waitFor({
+      state: "visible",
+      timeout: 10000,
+    });
     await this.emailInput.click();
     await this.emailInput.type(username);
   }
 
   async fillPassword(password) {
+    await this.passwordInput.waitFor({
+      state: "visible",
+      timeout: 10000,
+    });
     await this.passwordInput.click();
     await this.passwordInput.type(password);
   }
 
   async clickNext(){
+    await this.nextButton.waitFor({
+      state: "visible",
+      timeout: 10000,
+    });
     await this.nextButton.click();
   }
 
+  async clickPay(){
+    await this.payButton.waitFor({
+      state: "visible",
+      timeout: 10000,
+    });
+    await this.payButton.click();
+  }
+
   async payWithGoogle(username, password){
+    await this.page.waitForLoadState("networkidle", { timeout: 10000 });
+
     await this.fillUsername(username);
     await this.clickNext();
 
     await this.fillPassword(password);
     await this.clickNext();
+    await this.page.waitForLoadState("networkidle", { timeout: 10000 });
 
-    await this.page.waitForLoadState("networkidle", {timeout:10000});
+    await this.clickPay();
   }
 }
