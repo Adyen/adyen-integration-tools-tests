9270bfba67148a19b580ab48415bc09d76326735
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2023-08-29 16:38:59 +0200

    Instantiate page for QR Page

--- projects/common/redirect/QRPage.js
+++ projects/common/redirect/QRPage.js
@@ -2,6 +2,7 @@ import { expect } from "@playwright/test";
 
 export class QRPage {
     constructor(page) {
+        this.page = page;
         this.QRCodeWrapper = page.locator(".adyen-checkout__qr-loader");
         this.QRCode = this.QRCodeWrapper.locator("img[alt='Scan QR code']");
     }
