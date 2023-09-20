e041052ef7fa6d447889171cce868a147e574570
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2023-09-20 14:50:31 +0200

    Restructure test folders

--- projects/magento/tests/backoffice/Backoffice.spec.js
+++
@@ -1,8 +0,0 @@
-/* Since we use multiple workers for E2E tests and
-Magento doesn't allow multiple administrator sessions,
-we are running them through this file to allow serialization */
-
-import "./MOTO.js";
-import "./RequiredSettingsAutoConfig.js"
-import "./RequiredSettingsManualConfig.js"
-import "./PayByLink.js"
