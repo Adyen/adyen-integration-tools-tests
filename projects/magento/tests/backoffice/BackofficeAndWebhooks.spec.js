bc9817dbb1d57182419afb4ccfacf25b99076b30
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2023-12-19 15:46:52 +0100

    Change order of webhook and backoffice tests

--- projects/magento/tests/backoffice/BackofficeAndWebhooks.spec.js
+++ projects/magento/tests/backoffice/BackofficeAndWebhooks.spec.js
@@ -2,16 +2,16 @@
 Magento doesn't allow multiple administrator sessions,
 we are running them through this file to allow consequent runs */
 
-// Backoffice Tests
-import "./MOTO.js";
-import "./RequiredSettingsAutoConfig.js"
-import "./RequiredSettingsManualConfig.js"
-import "./PayByLink.js"
-
 /* Webhook Tests are also run through this file
 since they also require usage of Admin Panel */
 
 import "../webhook/Authorisation.js";
 import "../webhook/Capture.js";
 import "../webhook/Refund.js";
+
+// Backoffice Tests
+import "./MOTO.js";
+import "./RequiredSettingsAutoConfig.js"
+import "./RequiredSettingsManualConfig.js"
+import "./PayByLink.js"
 
