a66613261e2137402bd828f38abc6aa0ca86e065
Can Demiralp <ecandemiralp@gmail.com>
2023-12-29 13:47:07 +0100

    [ECP-8860] Use 3DS2 on Bancontact payments

--- projects/magento/tests/BancontactCardPayment.spec.js
+++ projects/magento/tests/BancontactCardPayment.spec.js
@@ -1,14 +1,13 @@
 import { test } from "@playwright/test";
-import { ThreeDSPaymentPage } from "../../common/redirect/ThreeDSPaymentPage.js";
 import PaymentResources from "../../data/PaymentResources.js";
 import {
   goToShippingWithFullCart,
-  placeOrder,
   proceedToPaymentAs,
-  verifyFailedPayment,
   verifySuccessfulPayment,
 } from "../helpers/ScenarioHelper.js";
 import { makeCreditCardPayment } from "../helpers/PaymentHelper.js";
+import {ThreeDS2PaymentPage} from "../../common/redirect/ThreeDS2PaymentPage.js";
+import {CreditCardComponentsMagento} from "../pageObjects/checkout/CreditCardComponentsMagento.js";
 
 const paymentResources = new PaymentResources();
 const bancontactCard = paymentResources.bcmc.be;
@@ -28,20 +27,18 @@ test.describe.parallel("Payment via Bancontact Card", () => {
   });
 
   test("should succeed with correct 3DS credentials", async ({ page }) => {
-    await new ThreeDSPaymentPage(page).validate3DS(
-      bancontactCard.user,
-      bancontactCard.password
+    await new ThreeDS2PaymentPage(page).validate3DS2(
+        paymentResources.threeDSCorrectPassword
     );
 
     await verifySuccessfulPayment(page);
   });
 
   test("should fail with wrong 3DS credentials", async ({ page }) => {
-    await new ThreeDSPaymentPage(page).validate3DS(
-      bancontactCard.wrongUser,
-      bancontactCard.wrongPassword
+    await new ThreeDS2PaymentPage(page).validate3DS2(
+        paymentResources.threeDSWrongPassword
     );
 
-    await verifyFailedPayment(page);
+    await new CreditCardComponentsMagento(page).verifyPaymentRefusal();
   });
 });
