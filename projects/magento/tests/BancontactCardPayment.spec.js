14c61c16e9aa40e920cdd9aaa558bbb7e07b415f
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2024-01-03 17:56:04 +0100

    Make the tests and assertions more robust

--- projects/magento/tests/BancontactCardPayment.spec.js
+++ projects/magento/tests/BancontactCardPayment.spec.js
@@ -4,6 +4,7 @@ import {
   goToShippingWithFullCart,
   placeOrder,
   proceedToPaymentAs,
+  verifyPaymentRefusal,
   verifySuccessfulPayment,
 } from "../helpers/ScenarioHelper.js";
 import { PaymentDetailsPage } from "../pageObjects/plugin/PaymentDetails.page.js";
@@ -27,21 +28,36 @@ test.describe.parallel("Payment via Bancontact Card", () => {
       user.lastName
     );
     await placeOrder(page);
+    await page.waitForLoadState("load", { timeout: 15000 });
   });
 
   test("should succeed with correct 3DS credentials", async ({ page }) => {
     const threeDS2Page = new ThreeDS2PaymentPage(page);
-    if (await threeDS2Page.threeDS2PasswordInput.isVisible({timeout:3000})){
-      await new threeDS2Page.validate3DS2(paymentResources.threeDSCorrectPassword);
+  
+    try {
+      // Attempt to validate with the correct 3DS credentials
+      await threeDS2Page.validate3DS2(paymentResources.threeDSCorrectPassword);
+    } catch (error) {
+      console.error("Error: ", error);
     }
+  
+    // Verify successful payment after trying to validate
     await verifySuccessfulPayment(page);
   });
 
   test("should fail with wrong 3DS credentials", async ({ page }) => {
     const threeDS2Page = new ThreeDS2PaymentPage(page);
-    if (await threeDS2Page.threeDS2PasswordInput.isVisible({timeout:3000})){
-      await new threeDS2Page.validate3DS2(paymentResources.threeDSWrongPassword);
+  
+    try {
+      // Attempt to validate with the wrong 3DS credentials
+      await threeDS2Page.validate3DS2(paymentResources.threeDSWrongPassword);
+      await threeDS2Page.threeDS2SubmitButton.waitFor({state:"hidden", timeout:10000})
+    } catch (error) {
+      console.error("Error: ", error);
     }
-    await new BancontactCardComponentsMagento(page).verifyPaymentRefusal();
+  
+    // Verify payment refusal regardless of the above outcome
+    await verifyPaymentRefusal(page);
   });
+  
 });
