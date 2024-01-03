d134852338087e7f5d2dd04b097b3d8fd5a3c950
Cenk Kucukiravul <cenk.kucukiravul@adyen.com>
2024-01-03 17:54:46 +0100

    Add new helper to verify refusal messages

--- projects/magento/helpers/ScenarioHelper.js
+++ projects/magento/helpers/ScenarioHelper.js
@@ -60,6 +60,13 @@ export async function verifySuccessfulPayment(page, redirect = true, timeout) {
   );
 }
 
+export async function verifyPaymentRefusal(page) {
+  const paymentDetailsPage = new PaymentDetailsPage(page);
+  expect(await paymentDetailsPage.errorMessageContainer.innerText()).toContain(
+    "The payment is REFUSED."
+    );
+}
+
 export async function getOrderNumber(page){
   return (await new SuccessfulCheckoutPage(page).orderNumber());
 }
