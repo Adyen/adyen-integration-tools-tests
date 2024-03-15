f418d3eb7d3c6bd7272188f6f56fa72050fbf93f
Cenk Kucukiravul <cenkiravul35@gmail.com>
2024-03-15 13:56:05 +0100

    Add Amazon sandbox page objects

---
+++ projects/common/redirect/AmazonPaymentPage.js
@@ -0,0 +1,27 @@
+export class AmazonPayPaymentPage {
+  constructor(page) {
+    this.page = page;
+
+    this.emailInput = this.page.locator("#ap_email");
+    this.passwordInput = this.page.locator("#ap_password");
+    this.loginButton = this.page.locator("#signInSubmit");
+    this.changePaymentButton = this.page.locator("#change-payment-button");
+    this.confirmPaymentChangeButton = this.page.locator("#a-autoid-8");
+    this.amazonCaptcha = this.page.locator('//img[contains(@alt,"captcha")]')
+
+  }
+
+  async proceedWithAmazonPay(username, password){
+
+    await this.emailInput.click();
+    await this.emailInput.type(username);
+    await this.passwordInput.click();
+    await this.passwordInput.type(password);
+    await this.loginButton.click();
+    await this.page.waitForLoadState("load", { timeout: 15000 });
+
+    if (await this.amazonCaptcha.isVisible()){
+      return false;
+    }
+  }
+}
