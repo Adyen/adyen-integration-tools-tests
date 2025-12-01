150b14699e92d1dae0cb93d8068ab36efff62ada
Can Demiralp <can.demiralp@adyen.com>
2025-12-01 13:57:02 +0100

    [ECP-9856] Allow normal test operation

--- projects/magento/tests/webhook/QueueProcessor.js
+++ projects/magento/tests/webhook/QueueProcessor.js
@@ -25,7 +25,7 @@ const headers = {
 
 let adminOrderCreationPage;
 
-test.only("Process AUTHORISATION webhook notifications", () => {
+test.describe("Process AUTHORISATION webhook notifications", () => {
   test.beforeEach(async ({ page }) => {
     await goToShippingWithFullCart(page,1);
     await proceedToPaymentAs(page, users.dutch);
