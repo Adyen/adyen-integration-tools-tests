f61297d6d33455654426a1cd2d461cd137400eb5
Khushboo <khushboo.singhvi@adyen.com>
2026-01-09 16:31:30 +0100

    [ECP-8975] Creating E2E test for Cancellation webhook (#165)
    * [ECP-8975] Creating E2E test for Cancellation webhook
    
    * [ECP-8975] Creating E2E test for Cancellation webhook
    
    * [ECP-8975] Creating E2E test for Cancellation webhook

--- projects/magento/tests/webhook/Authorisation.js
+++ projects/magento/tests/webhook/Authorisation.js
@@ -1,83 +1,17 @@
-import { expect, test } from "@playwright/test";
-import PaymentResources from "../../../data/PaymentResources.js";
+import { test, expect } from "@playwright/test";
 import SharedState from "./SharedState.js";
-import {
-  getOrderNumber,
-  goToShippingWithFullCart,
-  proceedToPaymentAs,
-  verifySuccessfulPayment,
-} from "../../helpers/ScenarioHelper.js";
-import { makeCreditCardPayment } from "../../helpers/PaymentHelper.js";
-
-const paymentResources = new PaymentResources();
-const webhookCredentials = paymentResources.webhookCredentials;
-const users = paymentResources.guestUser;
-let orderNumber;
-const randomPspNumber = Math.random().toString().slice(2,7);
-const username = webhookCredentials.webhookUsername;
-const password = webhookCredentials.webhookPassword;
-
-const base64Credentials = Buffer.from(`${username}:${password}`).toString('base64');
-const headers = {
-    Authorization: `Basic ${base64Credentials}`
-};
+import { createAuthorisedOrder } from "./createAuthorisedOrder.js";
 
 test.describe("Process AUTHORISATION webhook notifications", () => {
-  test.beforeEach(async ({ page }) => {
-    await goToShippingWithFullCart(page,1);
-    await proceedToPaymentAs(page, users.dutch);
-    await makeCreditCardPayment(
-      page,
-      users.regular,
-      paymentResources.visa,
-      paymentResources.expDate,
-      paymentResources.cvc
-    );
-    await verifySuccessfulPayment(page);
-    orderNumber = await getOrderNumber(page);
-    SharedState.orderNumber = orderNumber;
-  });
+    test("should be able to process AUTHORISATION notification", async ({ page, request }) => {
+        const { orderNumber, pspReference } = await createAuthorisedOrder({
+            page,
+            request,
+            flowKey: "authorisation",
+            sharedState: SharedState,
+        });
 
-  test("should be able to process AUTHORISATION notification", async ({ request }) => {
-   // Send the notification process request
-   const processWebhookResponse = await request.post("/adyen/webhook", {
-      headers,
-      data: {
-       "live" : "false",
-       "notificationItems" : [
-          {
-             "NotificationRequestItem" : {
-                   "amount" : {
-                   "currency" : "EUR",
-                   "value" : 7800
-                },
-                "eventCode" : "AUTHORISATION",
-                "eventDate" : "2023-05-23T15:48:53+02:00",
-                "merchantAccountCode" : `${paymentResources.apiCredentials.merchantAccount}`,
-                "merchantReference" : `${orderNumber}`,
-                "operations" : [
-                   "AUTHORISATION"
-                ],
-                "paymentMethod" : "visa",
-                "pspReference" : `LVL9PX2ZPQR${randomPspNumber}`,
-                "reason" : "",
-                "success" : "true"
-             }
-          }
-       ]
-    }
- });
- // Check response status
- expect(processWebhookResponse.status()).toBe(202);
-
- // Get processed notification
- const processedNotificationResponse = await request.get(`/adyentest/test?orderId=${orderNumber}&eventCode=AUTHORISATION`)
- 
- // Check response status
- expect(processedNotificationResponse.status()).toBe(200);
- 
- // Check the body of processed notification
- const processedNotificationBody = await processedNotificationResponse.json();
-  expect(processedNotificationBody[0].status).toBe("adyen_authorized");
- });
+        expect(orderNumber).toBeTruthy();
+        expect(pspReference).toBeTruthy();
+    });
 });
